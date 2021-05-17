22f467365635d634003e9112667e398e28aa8fe8
Chirag Bairwa <chiragbairwa2k18@gmail.com>
2021-05-17 14:28:12 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  let [red, setred] = useState(50)
+  let [green, setgreen] = useState(50)
+  let [blue, setblue] = useState(50)
+  
+  function changeColor(){
+    red = setred(document.getElementById('red').value)
+    green = setgreen(document.getElementById('green').value)
+    blue = setblue(document.getElementById('blue').value)
+  }
+  document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Card */}
+        <div style={{
+          backgroundColor: 'white', 
+          width:'10rem', 
+          padding: '10px',
+          textAlign:'center',
+          boxShadow: '0 4px 8px 0 rgba(0,0,0,0.5)',
+          transition: '0.3s'
+        }}>
+          <p>Red   :
+            <input type="range" min="0" max="255" id="red" onChange={changeColor} /></p>
+          <p>Green :
+            <input type="range" min="0" max="255" id="green" onChange={changeColor} /></p>
+          <p>Blue  :
+            <input type="range" min="0" max="255" id="blue" onChange={changeColor} /></p>
+          <p>rgb({red}, {green}, {blue})</p>
+        </div>
     </div>
   );
 }
-
-export default App;
