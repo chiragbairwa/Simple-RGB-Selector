534ae5fe4b7ce73d993133c4aafcac4e6428ec5a
Chirag Bairwa <chiragbairwa2k18@gmail.com>
2021-06-04 18:32:35 +0530

    ++materialize-css@next

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export default function App() {
   let [red, setred] = useState(50)
   let [green, setgreen] = useState(50)
   let [blue, setblue] = useState(50)
-  
-  function changeColor(){
+
+  function changeColor() {
     red = setred(document.getElementById('red').value)
     green = setgreen(document.getElementById('green').value)
     blue = setblue(document.getElementById('blue').value)
@@ -14,23 +14,25 @@ export default function App() {
 
   return (
     <div className="App">
-        {/* Card */}
-        <div style={{
-          backgroundColor: 'white', 
-          width:'10rem', 
-          padding: '10px',
-          textAlign:'center',
-          boxShadow: '0 4px 8px 0 rgba(0,0,0,0.5)',
-          transition: '0.3s'
-        }}>
-          <p>Red   :
+      {/* Card */}
+      <div style={{
+        width: '200px',
+        margin: '10px',
+        textAlign: 'left',
+        borderRadius:'5px',
+        padding: '8px 10px',
+        backgroundColor: 'white',
+        boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)',
+      }}>
+        <p>Red   :
             <input type="range" min="0" max="255" id="red" onChange={changeColor} /></p>
-          <p>Green :
+        <p>Green :
             <input type="range" min="0" max="255" id="green" onChange={changeColor} /></p>
-          <p>Blue  :
+        <p>Blue  :
             <input type="range" min="0" max="255" id="blue" onChange={changeColor} /></p>
-          <p>rgb({red}, {green}, {blue})</p>
-        </div>
+
+        <p align="center">rgb({red}, {green}, {blue})</p>
+      </div>
     </div>
   );
 }
